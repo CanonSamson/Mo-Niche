dc59ac5d8cfa8a7eb483358ced311ae482dfc7b2
CanonSamson <samsoncanon2018@gmail.com>
2024-03-16 10:39:26 +0100

    bug fix

--- app/products/category/[category]/page.jsx
+++ app/products/category/[category]/page.jsx
@@ -8,9 +8,14 @@ const Page = () => {
   const { category } = useParams();
   const [categoryProducts, setCategoryProducts] = useState(null);
   const { products } = useSelector((state) => state.app);
+  const [heading, setHeading] = useState([]);
 
   useEffect(() => {
     const getProducts = () => {
+      const heading = category?.split("-");
+
+      setHeading(heading);
+      console.log(heading);
       const categoryProducts = products.filter(
         (item) => item.category === category
       );
@@ -23,8 +28,12 @@ const Page = () => {
     <main className=" px-2 mt-20">
       <section className="mt-20  ">
         <div className="  items-center flex flex-col  justify-center ">
-          <h3>Shop our bestsellers</h3>
-          <p className=" border-b border-black">View all</p>
+          <h3>
+            {heading?.map((item, index) => (
+              <span key={index}>{item} {" "}</span>
+            ))}
+          </h3>
+          <p className=" border-b border-black">All products</p>
         </div>
 
         <div className="  grid grid-cols-2 gap-1 mt-4">
